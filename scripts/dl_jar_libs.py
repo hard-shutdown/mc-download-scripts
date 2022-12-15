a4d737ec11db0229564d6856c45f28e29a9d9d9b
Cody <59001842+hard-shutdown@users.noreply.github.com>
2022-12-15 23:05:59 +0000

    slight changes

--- scripts/dl_jar_libs.py
+++ scripts/dl_jar_libs.py
@@ -17,12 +17,14 @@ def main():
     tmpdir = (os.getenv("TEMP") if os.name=="nt" else "/tmp") + "/" + str(random.uniform(1, 100000))
     os.mkdir(tmpdir)
     os.chdir(tmpdir)
+    with open(sys.argv[1].replace(".", "-") + "_manifest.json", "w", encoding='utf8') as f:
+        f.write(str(version_manifest))
     dl_all_libs(libs, True)
     print("Downloading " + sys.argv[1] + " client jar")
     download_jar(man, sys.argv[1].replace(".", "-") + "_client.jar", sys.argv[1], "client")
-    with zipfile.ZipFile(actualdir + "/" + sys.argv[1].replace(".", "-") + "_client.zip", "w") as zip:
+    with zipfile.ZipFile(actualdir + "/" + sys.argv[1].replace(".", "-") + "_client.zip", "w", compression=zipfile.ZIP_BZIP2, compresslevel=9) as zf:
         for file in os.listdir(tmpdir):
-            zip.write(file)
+            zf.write(file)
     os.chdir(actualdir)
     shutil.rmtree(tmpdir)
 
